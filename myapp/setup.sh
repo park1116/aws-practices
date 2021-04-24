5736ad3cda83ab4c258620f8c92d25978abb7ad6
park1116 <park_1116@naver.com>
2021-04-24 17:11:43 +0900

    myapp setup script

---
+++ myapp/setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo yum install git -y
+sudo curl --silent --location https://rpm.nodesource.com/setup_7.x|bash -
+sudo yum -y install nodejs
+
+git clone https://github.com/kickscar/aws-practices.git
+node aws-practices/myapp/index.js &
