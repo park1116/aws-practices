c3bd1b3d3aefe2d92b28259c231881d09dc67102
park1116 <park_1116@naver.com>
2021-05-15 17:48:28 +0900

    210508

---
+++ ch05/02/security-update.sh
@@ -0,0 +1,8 @@
+PUBLICNAMES=$(aws ec2 describe-instances \
+--filters "Name=instance-state-name,Values=running" \
+--query "Reservations[].Instances[].PublicDnsName" \
+--output text)
+
+for PUBLICNAME in $PUBLICNAMES; do
+	ssh -i ~/mykey2.pem ec2-user@$PUBLICNAME "sudo yum -y --security update"
+done
